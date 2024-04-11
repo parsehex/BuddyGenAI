1a740c2fb57ffb511c56c118dd6184ae70509550
parsehex <1779937+parsehex@users.noreply.github.com>
2024-04-11 13:54:50 -0400

    add basic persona viewing and editing

--- pages/persona/[id]/edit.vue
+++ pages/persona/[id]/edit.vue
@@ -1,14 +1,52 @@
 <script setup lang="ts">
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { Button } from '~/components/ui/button';
+import { Input } from '~/components/ui/input';
+import { Textarea } from '~/components/ui/textarea';
+import { getPersona, updatePersona } from '~/lib/api/persona';
+import type { Persona } from '~/server/database/knex.d';
+
 const route = useRoute();
 const id = route.params.id as string;
+
+const persona = ref(null as Persona | null);
+
+const nameValue = ref('');
+const descriptionValue = ref('');
+
+onBeforeMount(async () => {
+	persona.value = (await getPersona(id)).value;
+	nameValue.value = persona.value?.name ?? '';
+	descriptionValue.value = persona.value?.description ?? '';
+});
+
+const handleSave = async () => {
+	await updatePersona({
+		id: +id,
+		name: nameValue.value,
+		description: descriptionValue.value,
+	});
+	await navigateTo(`./view`);
+};
 </script>
 
 <template>
-	<Suspense>
-		<div class="container flex">
-			{{ id }}
+	<div class="container flex flex-col items-center">
+		<h1 class="text-2xl font-bold">
+			Edit Persona
+			<NuxtLink class="ml-4" to="./view">View</NuxtLink>
+		</h1>
+		<div class="w-full md:w-1/2 mb-4">
+			<Input v-model="nameValue" placeholder="Persona name" />
 		</div>
-	</Suspense>
+		<Card class="w-full md:w-1/2">
+			<CardHeader class="text-lg">Description</CardHeader>
+			<CardContent>
+				<Textarea v-model="descriptionValue" placeholder="Persona description" />
+			</CardContent>
+		</Card>
+		<Button @click="handleSave">Save</Button>
+	</div>
 </template>
 
 <style>
--- pages/persona/[id]/view.vue
+++ pages/persona/[id]/view.vue
@@ -1,20 +1,35 @@
 <script setup lang="ts">
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import Button from '~/components/ui/button/Button.vue';
+import { getPersona } from '~/lib/api/persona';
+import type { Persona } from '~/server/database/knex.d';
+
 const route = useRoute();
 const id = route.params.id as string;
+
+const persona = ref(null as Persona | null);
+
+onBeforeMount(async () => {
+	persona.value = (await getPersona(id)).value;
+});
 </script>
 
 <template>
-	<Suspense>
-		<div class="container flex">
-			{{ id }}
-		</div>
-	</Suspense>
+	<div class="container flex flex-col items-center">
+		<h1 class="text-2xl font-bold">
+			Persona: <span class="text-blue-500">{{ persona?.name }}</span>
+			<NuxtLink class="ml-4" to="./edit">Edit</NuxtLink>
+		</h1>
+		<Card class="w-full md:w-1/2">
+			<CardHeader class="text-lg"> Description </CardHeader>
+			<CardContent>
+				{{ persona?.description }}
+				<span v-if="`${persona?.description}`.length === 0" class="text-gray-400 italic">No description</span>
+			</CardContent>
+		</Card>
+	</div>
 </template>
 
 <style>
-#electron-status {
-	position: absolute;
-	font-size: 2rem;
-	font: bold;
-}
+/*  */
 </style>
