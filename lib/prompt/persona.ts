3e0464951688594f22eca2e8c61cc282204191b9
parsehex <1779937+parsehex@users.noreply.github.com>
2024-04-13 19:41:23 -0400

    update persona prompt

--- lib/prompt/persona.ts
+++ lib/prompt/persona.ts
@@ -1,7 +1,7 @@
 export function promptFromPersonaDescription(name: string, description: string) {
 	console.log('promptFromPersonaDescription', name, description);
 	if (!description) {
-		return `The following is a chat between a User and an Assistant faithfully playing the role of ${name}.`;
+		return `The following is a chat between a User and an Assistant dilligantly playing the role of ${name}.`;
 	}
-	return `The following is a chat between a User and an Assistant playing the role of ${name}. Description of ${name} that Assistant must follow to its best ability and without hesitation:\n${description}`;
+	return `The following is a chat between a User and an Assistant playing the role of ${name}. Description of ${name} that Assistant dilligantly follows to its best ability and without hesitation:\n${description}`;
 }
