f98d9d50f9b396c78e2bbcf8f1cbfaeb857e78a7
parsehex <1779937+parsehex@users.noreply.github.com>
2024-05-17 20:53:43 -0400

    add "AI Assistant" option to buddy-select

--- components/BuddySelect.vue
+++ components/BuddySelect.vue
@@ -14,16 +14,28 @@ import { useAppStore } from '~/stores/main';
 const buddies = useAppStore().buddies as BuddyVersionMerged[];
 
 const model = defineModel<string>({ required: false });
+
+defineProps({
+	modelValue: {
+		type: String,
+		required: false,
+	},
+	includeAi: {
+		type: Boolean,
+		default: false,
+	},
+});
 </script>
 
 <template>
 	<Select v-model:model-value="model" class="my-2">
 		<SelectTrigger>
-			<SelectValue placeholder="Select a Buddy" />
+			<SelectValue placeholder="Start a chat with..." />
 		</SelectTrigger>
 		<SelectContent>
 			<SelectLabel>Buddies</SelectLabel>
 			<SelectGroup>
+				<SelectItem v-if="includeAi" value="ai"> AI Assistant </SelectItem>
 				<SelectItem v-for="buddy in buddies" :key="buddy.id" :value="buddy.id">
 					{{ buddy.name }}
 				</SelectItem>
