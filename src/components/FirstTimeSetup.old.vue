d6954b6c478d33fbbf9ee556a40c37498a1005f7
parsehex <1779937+parsehex@users.noreply.github.com>
2024-05-21 18:49:26 -0400

    swap new&old FirstTimeSetup components
    new one disables external model option

--- src/components/FirstTimeSetup.old.vue
+++ src/components/FirstTimeSetup.old.vue
@@ -378,6 +378,88 @@ const acceptPicKeywords = () => {
 				Getting ready...
 			</div>
 
+			<!-- TODO implement external models using OpenRouter (idk how for images) -->
+			<Card
+				v-if="!store.isModelsSetup"
+				class="whitespace-pre-wrap w-full p-2 pt-4 pb-0"
+			>
+				<CardHeader class="pt-0 pb-0 text-center">
+					Welcome! Please answer a few questions before we get started. First...
+				</CardHeader>
+				<CardContent>
+					<div
+						class="flex flex-col w-full items-center justify-between gap-1.5 mt-4 pb-0"
+					>
+						<h2 class="text-lg text-center">
+							Would you like to use <b>External (OpenAI)</b> or <b>Local</b> Models?
+						</h2>
+						<ul class="my-2">
+							<li>
+								<u>External</u>
+								-- currently only OpenAI/ChatGPT is supported
+							</li>
+							<li>
+								<u>Local</u>
+								-- uses
+								<span
+									class="text-blue-500 cursor-pointer hover:underline"
+									@click="
+										openExternalLink &&
+											openExternalLink('https://github.com/Mozilla-Ocho/llamafile')
+									"
+								>
+									{{ 'LlamaFile' }}
+								</span>
+								+
+								<span
+									class="text-blue-500 cursor-pointer hover:underline"
+									@click="
+										openExternalLink &&
+											openExternalLink('https://github.com/leejet/stable-diffusion.cpp')
+									"
+								>
+									{{ 'Stable-Diffusion.cpp' }}
+								</span>
+							</li>
+						</ul>
+
+						<div class="flex items-center justify-center gap-2">
+							<Button
+								@click="
+									() => {
+										selectedModelProvider = 'external';
+										modelProvider = 'external';
+									}
+								"
+								:variant="selectedModelProvider === 'external' ? 'default' : 'outline'"
+							>
+								External
+							</Button>
+							<Button
+								@click="
+									() => {
+										selectedModelProvider = 'local';
+										modelProvider = 'local';
+									}
+								"
+								:variant="selectedModelProvider === 'local' ? 'default' : 'outline'"
+							>
+								Local
+							</Button>
+						</div>
+					</div>
+				</CardContent>
+			</Card>
+
+			<ExternalModelSettingsCard
+				v-if="
+					!store.isModelsSetup &&
+					selectedModelProvider &&
+					modelProvider === 'external'
+				"
+				:first-time="store.newHere"
+			/>
+
 			<LocalModelSettingsCard
 				v-if="
 					!store.isModelsSetup && selectedModelProvider && modelProvider === 'local'
