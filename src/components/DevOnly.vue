e6a631bf963db7832b9c2aa9bfc4e92119d019f8
parsehex <1779937+parsehex@users.noreply.github.com>
2024-05-30 13:39:30 -0400

    style dev-only components

--- src/components/DevOnly.vue
+++ src/components/DevOnly.vue
@@ -3,7 +3,12 @@ import { isDevMode } from '@/lib/utils';
 </script>
 
 <template>
-	<div v-if="isDevMode()">
+	<div
+		v-if="isDevMode()"
+		class="opacity-75 hover:opacity-100 p-2 m-2 bg-orange-200 rounded-md border border-orange-400 font-mono"
+		title="This component is only visible in development mode."
+	>
+		<span class="text-black font-bold select-none">🛠️ Dev Only:</span>
 		<slot></slot>
 	</div>
 </template>
