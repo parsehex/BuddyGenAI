d26ffb7d8c8ed2b416ede54ae59abff4ecbca2d7
parsehex <1779937+parsehex@users.noreply.github.com>
2024-09-14 16:00:06 -0400

    add note

--- src/components/BuddySelect.vue
+++ src/components/BuddySelect.vue
@@ -27,6 +27,12 @@ defineProps({
 });
 </script>
 
+<!-- change to a combo button labelled "New Chat:"
+- Has option "Assistant" and a dropdown button with buddies
+- Choosing a buddy or clicking "Assistant" will start a chat with that buddy or the AI assistant
+- No need for button to start a chat, 1 click is enough
+-->
+
 <template>
 	<Select v-model:model-value="model" class="my-2">
 		<SelectTrigger>
