eb3edf9cd85338a615721a9364b2c0433558be34
parsehex <parsehex@users.noreply.github.com>
2025-01-29 03:16:57 +0000

    deploy: 46b8053eb4672a874bc0a58798c183f68d99868b

--- assets/CardHeader.vue_vue_type_script_setup_true_lang-DI4NSll4.js
+++
@@ -1,1 +0,0 @@
-import{d as o,o as r,b as n,O as l,a8 as c,e as t,$ as p}from"./index-CJEy3eNw.js";const m=o({__name:"CardHeader",props:{class:{}},setup(e){const s=e;return(a,d)=>(r(),n("div",{class:c(t(p)("flex flex-col gap-y-1.5 p-6",s.class))},[l(a.$slots,"default")],2))}});export{m as _};
