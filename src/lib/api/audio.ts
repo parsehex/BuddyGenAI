cce51ea34a2a0511bb4132551d290a1ccf91b42c
parsehex <1779937+parsehex@users.noreply.github.com>
2025-08-17 00:16:57 -0400

    missing import

--- src/lib/api/audio.ts
+++ src/lib/api/audio.ts
@@ -1,9 +1,12 @@
 import useElectron from '@/src/composables/useElectron';
 import { select } from '../sql';
+import { blobToBase64 } from '../utils';
 
 const { dbGet } = useElectron();
 
 export async function getAudio(id: string) {
+	if (id.includes('data:')) return id;
+
 	const sql = select('audio', ['*'], { id });
 	const audio = (await dbGet(sql[0], sql[1]));
 
