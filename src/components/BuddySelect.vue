c7baacce3b546fa95f3e6767cff192d83ec93f92
parsehex <1779937+parsehex@users.noreply.github.com>
2025-08-17 00:06:09 -0400

    add floating reference card to buddy select

--- src/components/BuddySelect.vue
+++ src/components/BuddySelect.vue
@@ -16,10 +16,16 @@ import {
 	SelectTrigger,
 	SelectValue,
 } from '@/components/ui/select';
+import {
+	HoverCard,
+	HoverCardContent,
+	HoverCardTrigger,
+} from '@/components/ui/hover-card';
 import { ChevronDown } from 'lucide-vue-next';
 import type { BuddyVersionMerged } from '@/lib/api/types-db';
 import { useAppStore } from '@/stores/main';
 import { Label } from './ui/label';
+import BuddyAvatar from './BuddyAvatar.vue';
 
 const selectedBuddy = ref('');
 
@@ -37,37 +43,44 @@ const props = defineProps({
 const startChat = (id: string | null) => {
 	emit('select', id);
 };
-</script>
 
+const truncateDescription = (description: string, maxLength: number) => {
+	if (description.length <= maxLength) {
+		return description;
+	}
+	return description.substring(0, maxLength) + '...';
+};
+</script>
 <template>
 	<div class="flex items-center space-x-2 justify-around w-full">
 		<Label>Chat with:</Label>
 		<Button v-if="includeAi" @click="startChat('ai')"> AI Assistant </Button>
-		<Select
-			v-if="store.buddies.length > 0"
-			class="my-2"
-			@update:modelValue="
-				(id) => {
-					startChat(id);
-					selectedBuddy = '';
-				}
-			"
-			v-model="selectedBuddy"
-		>
+		<Select v-if="store.buddies.length > 0" class="my-2" @update:modelValue="
+			(id) => {
+				startChat(id);
+				selectedBuddy = '';
+			}
+		" v-model="selectedBuddy">
 			<SelectTrigger>
 				<SelectValue placeholder="Buddy" />
 			</SelectTrigger>
 			<SelectContent>
 				<SelectLabel>Buddies</SelectLabel>
 				<SelectGroup>
-					<SelectItem
-						v-for="buddy in store.buddies"
-						:key="buddy.id"
-						:value="buddy.id"
-						@click="startChat(buddy.id)"
-					>
-						{{ buddy.name }}
-					</SelectItem>
+					<HoverCard v-for="buddy in store.buddies" :key="buddy.id" :open-delay="200">
+						<HoverCardTrigger as-child>
+							<SelectItem :value="buddy.id" @click="startChat(buddy.id)"> {{ buddy.name }} </SelectItem>
+						</HoverCardTrigger>
+						<HoverCardContent class="w-80" side="right">
+							<div class="flex justify-around space-x-4">
+								<BuddyAvatar :buddy="buddy" class="h-12 w-12" />
+								<div class="space-y-1 grow">
+									<h4 class="text-sm font-semibold">{{ buddy.name }}</h4>
+									<p class="text-sm"> {{ truncateDescription(buddy.description || '', 150) }} </p>
+								</div>
+							</div>
+						</HoverCardContent>
+					</HoverCard>
 				</SelectGroup>
 			</SelectContent>
 		</Select>
