9bdd1754c29ff4693cd534f813e6140fc3a3c9fd
parsehex <1779937+parsehex@users.noreply.github.com>
2025-08-20 10:58:07 -0400

    more deps cleanup
    - use node v20
    - update vue router
    - update vue-router auto-import exclude pattern
    - remove fs-extra, openai packages

--- src/lib/router.ts
+++ src/lib/router.ts
@@ -1,7 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router/auto';
+import { createRouter, createWebHistory } from 'vue-router';
+import { routes, handleHotUpdate } from 'vue-router/auto-routes';
 
-export default createRouter({
+const router = createRouter({
 	history: createWebHistory(),
-	// You don't need to pass the routes anymore,
-	// the plugin writes it for you 🤖
+	routes,
 });
+
+export default router;
+
+// @ts-ignore
+if (import.meta.hot) {
+	handleHotUpdate(router);
+}
